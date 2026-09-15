cc1b5eea502b5e67a33a0f464ebe45be43267f13
fajriantomanungki <73055715+fajriantomanungki@users.noreply.github.com>
2026-09-16 00:45:37 +0800

    fix: align maps compose with Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,7 +94,7 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
-    implementation("com.google.maps.android:maps-compose:6.7.0")
+    implementation("com.google.maps.android:maps-compose:6.4.1")
     implementation("androidx.camera:camera-camera2:1.4.1")
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
