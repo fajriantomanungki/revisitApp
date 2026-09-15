0b56033758a9a554dac5518d15df92d2feafe3da
fajriantomanungki <73055715+fajriantomanungki@users.noreply.github.com>
2026-09-16 00:56:04 +0800

    fix: opt in wilayah selector material api

--- app/src/main/java/com/fajriantomanungki/revisitapp/feature/wilayah/WilayahSelectionUi.kt
+++ app/src/main/java/com/fajriantomanungki/revisitapp/feature/wilayah/WilayahSelectionUi.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
 package com.fajriantomanungki.revisitapp.feature.wilayah
 
 import androidx.compose.foundation.layout.Arrangement
